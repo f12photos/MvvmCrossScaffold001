b60aa37d12f19d871b3e316f179b51077de88ac1
Minh Nguyen <mechpencilno2@gmail.com>
2019-04-30 22:05:54 -0500

    wip

--- src/ConsoleApp1/Program.cs
+++ src/ConsoleApp1/Program.cs
@@ -11,7 +11,31 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            SQLiteConnection sqliteconn = new SQLiteConnection("")
+
+
+            using (SQLiteConnection sqliteconn = new SQLiteConnection("chinook.db"))
+            {
+
+            }
+        }
+
+        static SQLiteConnection CreateConnection()
+        {
+
+            SQLiteConnection sqlite_conn;
+            // Create a new database connection:
+            sqlite_conn = new SQLiteConnection("Data Source=database.db; Version = 3; New = True; Compress = True; ");
+
+            // Open the connection:
+            try
+            {
+                sqlite_conn.();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return sqlite_conn;
         }
     }
 }
