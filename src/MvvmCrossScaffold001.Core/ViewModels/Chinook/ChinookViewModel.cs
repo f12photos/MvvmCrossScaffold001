733c94413a373f1627d8967cb6dcdbf3790d6bcf
Minh Nguyen <mechpencilno2@gmail.com>
2019-05-12 20:32:35 -0500

    WIP

---
+++ src/MvvmCrossScaffold001.Core/ViewModels/Chinook/ChinookViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MvvmCrossScaffold001.Core.ViewModels.Chinook
+{
+    public class ChinookViewModel : BaseViewModel
+    {
+        public ChinookViewModel()
+        {
+
+        }
+    }
+}
