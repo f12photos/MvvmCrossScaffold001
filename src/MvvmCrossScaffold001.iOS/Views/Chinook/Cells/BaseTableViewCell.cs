5901be267ad85559c7ca6978c6c72278f7c69561
Minh Nguyen <mechpencilno2@gmail.com>
2019-05-19 07:08:20 -0400

    wip

---
+++ src/MvvmCrossScaffold001.iOS/Views/Chinook/Cells/BaseTableViewCell.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using MvvmCross.Platforms.Ios.Binding.Views;
+
+namespace MvvmCrossScaffold001.iOS.Views.Chinook.Cells
+{
+    public class BaseTableViewCell : MvxTableViewCell
+    {
+        private bool _didSetupConstraints;
+
+        public BaseTableViewCell(IntPtr handle) : base(handle)
+        {
+            RunLifecycle();
+        }
+
+        private void RunLifecycle()
+        {
+            CreateView();
+
+            SetNeedsUpdateConstraints();
+        }
+
+        public override sealed void UpdateConstraints()
+        {
+            if (!_didSetupConstraints)
+            {
+                LayoutView();
+
+                _didSetupConstraints = true;
+            }
+            base.UpdateConstraints();
+        }
+
+        protected virtual void CreateView()
+        {
+        }
+
+        protected virtual void LayoutView() // CreateConstraints()
+        {
+        }
+
+        protected virtual void BindView() // CreateConstraints()
+        {
+        }
+    }
+}
---
+++ src/MvvmCrossScaffold001.iOS/Views/Chinook/Cells/NameTableViewCell.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Cirrious.FluentLayouts.Touch;
+using MvvmCross.Binding.BindingContext;
+using MvvmCross.Platforms.Ios.Binding.Views;
+using UIKit;
+
+namespace MvvmCrossScaffold001.iOS.Views.Chinook.Cells
+{
+    /*
+     * CreateView();
+
+            LayoutView();
+
+            BindView();
+            */
+    
+
+    public class NameTableViewCell : BaseTableViewCell
+    {
+        private const float PADDING = 12f;
+
+        private UILabel _lblName;
+
+ 
+        public NameTableViewCell(IntPtr handle) : base(handle)
+        {
+            CreateView();
+
+            LayoutView();
+
+            BindView();
+        }
+
+        protected override void CreateView()
+        {
+            base.CreateView();
+
+            SelectionStyle = UITableViewCellSelectionStyle.None;
+
+            _lblName = new UILabel
+            {
+                TextColor = UIColor.Red,
+                Font = UIFont.SystemFontOfSize(15f, UIFontWeight.Bold)
+            };
+
+            BackgroundColor = UIColor.Clear;
+            ContentView.AddSubview(_lblName);
+            ContentView.SubviewsDoNotTranslateAutoresizingMaskIntoConstraints();
+
+            BindView();
+        }
+
+        protected override void LayoutView()
+        {
+            base.LayoutView();
+
+            ContentView.AddConstraints(
+                _lblName.AtLeftOf(ContentView, PADDING),
+                _lblName.AtTopOf(ContentView, PADDING),
+                _lblName.AtBottomOf(ContentView, PADDING),
+                _lblName.AtRightOf(ContentView, PADDING)
+            );
+        }
+
+        protected override void BindView()
+        {
+            this.DelayBind(
+                () =>
+                {
+                    this.AddBindings(_lblName, "Text Name");
+                });
+
+            //this.DelayBind(() =>
+            //{
+            //    var set = this.CreateBindingSet<HoursEntryCell, EnterTime>();
+            //    set.Bind(jobId).To(vm => vm.JobId);
+            //    set.Bind(hours).To(vm => vm.Hours);
+            //    set.Bind(jobName).To(vm => vm.JobName);
+            //    set.Apply();
+            //});
+        }
+    }
+}
