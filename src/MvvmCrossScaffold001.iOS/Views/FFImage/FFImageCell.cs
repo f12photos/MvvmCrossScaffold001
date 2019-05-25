a6f6e6ba9d52970a69635b4cee88751793b4314d
Minh Nguyen <mechpencilno2@gmail.com>
2019-05-25 11:54:20 -0500

    Updated cell size

--- src/MvvmCrossScaffold001.iOS/Views/FFImage/FFImageCell.cs
+++ src/MvvmCrossScaffold001.iOS/Views/FFImage/FFImageCell.cs
@@ -12,15 +12,21 @@ namespace MvvmCrossScaffold001.iOS.Views.FFImage
     public class FFImageCell : MvxTableViewCell
     {
         private MvxCachedImageView _imageControl;
-
+        private UILabel _labelWelcome;
         private bool _constraintsCreated;
 
         public FFImageCell(IntPtr handle) : base(handle)
         {
             _imageControl = new MvxCachedImageView();
             _imageControl.BackgroundColor = UIColor.Red;
-
             ContentView.AddSubview(_imageControl);
+
+            _labelWelcome = new UILabel
+            {
+                TextAlignment = UITextAlignment.Center
+            };
+            ContentView.AddSubview(_labelWelcome);
+
             ContentView.SubviewsDoNotTranslateAutoresizingMaskIntoConstraints();
 
             SetNeedsUpdateConstraints();
@@ -36,20 +42,27 @@ namespace MvvmCrossScaffold001.iOS.Views.FFImage
                     set.Bind(_imageControl).For(v => v.DownsampleWidth).To(vm => vm.DownsampleWidth);
                     //set.Bind(_imageControl).For(v => v.Transformations).To(vm => vm.Transformations);
                     set.Bind(_imageControl).For(v => v.ImagePath).To(vm => vm.Url);
+                    set.Bind(_labelWelcome).To(vm => vm.Url);
                     set.Apply();
                 }
             );
         }
 
         public override void UpdateConstraints()
-        {
+        { 
+            var margin = 10f;
             if (!_constraintsCreated)
             {
                 ContentView.AddConstraints(
-                    _imageControl.WithSameCenterY(ContentView),
-                    _imageControl.WithSameCenterX(ContentView),
-                    _imageControl.Width().EqualTo(55f),
-                    _imageControl.Height().EqualTo(55f)
+                    _imageControl.AtLeftOfSafeArea(ContentView, 2 * margin),
+                    _imageControl.AtTopOfSafeArea(ContentView, margin),
+                    _imageControl.AtBottomOfSafeArea(ContentView, margin),
+                    _imageControl.Width().EqualTo(100f),
+                    _imageControl.Height().EqualTo(100f),
+
+                    _labelWelcome.ToRightOf(_imageControl, margin),
+                    _labelWelcome.AtRightOfSafeArea(ContentView, 2 * margin),
+                    _labelWelcome.WithSameCenterY(_imageControl)
                 );
 
                 _constraintsCreated = true;
