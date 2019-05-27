1955d6cc70f0cc12b003b3ea009cbfe75f5c402c
Minh Nguyen <mechpencilno2@gmail.com>
2019-05-26 19:23:06 -0500

    Started adding Unit Testing

---
+++ MvvmCrossScaffold001.Core.Test/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using MvvmCrossScaffold001.Core.Services;
+using NUnit.Framework;
+
+namespace MvvmCrossScaffold001.Core.Test
+{
+    [TestFixture]
+    public class TipServiceTests
+    {
+        [Test]
+        public void TestThatZeroGenerosityReturnsZeroTip()
+        {
+            // Arrange
+            var tip = new CalculationService();
+
+            // Act
+            var result = tip.TipAmount(42.35, 0);
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void TestThatTenGenerosityReturnsTenPercentTip()
+        {
+            // Arrange
+            var tip = new CalculationService();
+
+            // Act
+            var result = tip.TipAmount(42.35, 10);
+
+            // Assert
+            Assert.AreEqual(4.235, result);
+        }
+    }
+}
