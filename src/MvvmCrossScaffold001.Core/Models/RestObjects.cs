7e78876cdfe98cfc11b7e040e0ba1df55b718d04
Minh Nguyen <mechpencilno2@gmail.com>
2019-05-27 22:50:21 -0500

    Added streaming to rest api

--- src/MvvmCrossScaffold001.Core/Models/RestObjects.cs
+++ src/MvvmCrossScaffold001.Core/Models/RestObjects.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
 namespace MvvmCrossScaffold001.Core.Models
 {
     public class UserRequest
@@ -14,5 +18,27 @@ namespace MvvmCrossScaffold001.Core.Models
         public string Body { get; set; }
         public int UserId { get; set; }
         public int Id { get; set; }
+    }
+
+    public class UploadedFile
+    {
+        public string FileFullName { get; set; }
+        public byte[] Data { get; set; }
+
+        public UploadedFile(string filePath)
+        {
+            FileFullName = Path.GetFileName(Normalize(filePath));
+            Data = File.ReadAllBytes(filePath);
+        }
+
+        private string Normalize(string input)
+        {
+            return new string(input
+                    .Normalize(System.Text.NormalizationForm.FormD)
+                    .Replace(" ", string.Empty)
+                    .ToCharArray()
+                    .Where(c => CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark)
+                    .ToArray());
+        }
     }
 }
--- src/MvvmCrossScaffold001.Core/Rest/HttpService.cs
+++ src/MvvmCrossScaffold001.Core/Rest/HttpService.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.IO;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using ModernHttpClient;
 using Newtonsoft.Json;
@@ -40,14 +44,32 @@ namespace MvvmCrossScaffold001.Core.Rest
 
         public Task<HttpResponseMessage> PostAsync(string url, object obj = null)
         {
+            //https://forums.xamarin.com/discussion/64176/how-to-upload-image-to-the-server-using-api-in-xamarin-forms
+            //https://stackoverflow.com/questions/53740665/send-image-with-httpclient-to-post-webapi-and-consume-the-data
+            //https://johnthiriet.com/efficient-post-calls/#
             //throw new NotImplementedException();
             using (var client = new HttpClient(nativeHandler))
             {
                 client.Timeout = TimeSpan.FromSeconds(30);
                 client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-                string jsonData = JsonConvert.SerializeObject(obj);
 
-                var content = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json");
+                //MultipartFormDataContent content = new MultipartFormDataContent();
+
+                //var upfilebytes = File.ReadAllBytes(file);
+                //ByteArrayContent baContent = new ByteArrayContent(upfilebytes);
+                //content.Add(baContent, "File", "filename.ext");
+
+                //StringContent studentIdContent = new StringContent("2123");
+                //content.Add(studentIdContent, "StudentId");
+
+                string jsonData = JsonConvert.SerializeObject(obj);
+                //StringContent strContent = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json");
+                //content.Add(strContent);
+
+                //var content = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json");
+
+                var content = CreateHttpContent(obj);
+
                 var response = client.PostAsync(url, content);
                 return response;
             }
@@ -67,5 +89,52 @@ namespace MvvmCrossScaffold001.Core.Rest
                 return response;
             }
         }
+
+        //======================================================================
+        //private async Task PostStreamAsync(object content, CancellationToken cancellationToken)
+        //{
+        //    using (var client = new HttpClient())
+        //    using (var request = new HttpRequestMessage(HttpMethod.Post, Url))
+        //    using (var httpContent = CreateHttpContent(content))
+        //    {
+        //        request.Content = httpContent;
+
+        //        using (var response = await client
+        //            .SendAsync(request, HttpCompletionOption.ResponseHeadersRead, cancellationToken)
+        //            .ConfigureAwait(false))
+        //        {
+        //            response.EnsureSuccessStatusCode();
+        //        }
+        //    }
+        //}
+
+        private HttpContent CreateHttpContent(object content)
+        {
+            HttpContent httpContent = null;
+
+            if (content != null)
+            {
+                var ms = new MemoryStream();
+                SerializeJsonIntoStream(content, ms);
+                ms.Seek(0, SeekOrigin.Begin);
+                httpContent = new StreamContent(ms);
+                httpContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            }
+
+            return httpContent;
+        }
+
+        public void SerializeJsonIntoStream(object value, Stream stream)
+        {
+            using (var sw = new StreamWriter(stream, new UTF8Encoding(false), 1024, true))
+            using (var jtw = new JsonTextWriter(sw) { Formatting = Formatting.None })
+            {
+                var js = new JsonSerializer();
+                js.Serialize(jtw, value);
+                jtw.Flush();
+            }
+        }
+
+
     }
 }
